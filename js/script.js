6e6d24317bf069cf33c04a57297d1f62574e4e05
lucasgar98 <lucas_garbarino@hotmail.com>
2025-12-01 19:28:29 -0300

    Se agregó el código necesario para manejar el inicio de sesión.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//---------- WEBSOCKETS -----------
 // Establecemos una nueva conexión websocket con Node-RED (el cual escucha en el puerto 1880)
 let socket = new WebSocket("ws://localhost:1880/ws");
 
@@ -49,3 +50,45 @@ socket.onclose = function() {
 socket.onerror = function(error) {
     console.error("Error en WebSocket: ", error);
 };
+
+//localStorage.removeItem("sesionIniciada");
+/* Definimos un evento que se dispara cuando se intenta ingresar a la sección de datos
+sin haber iniciado sesión */
+let linkDatos = document.getElementById("datos-link");
+linkDatos.addEventListener("click", function() {
+    // Verificamos si se inició sesión
+    const sesionIniciada = sessionStorage.getItem("sesionIniciada");
+    console.log(sesionIniciada);
+    if(sesionIniciada == "true"){
+        // Redirigimos a datos.html en caso que se haya iniciado sesión
+        window.location.href = "pages/datos.html";
+    }
+    else if(sesionIniciada == "false" || sesionIniciada == null) {
+        // Definimos la página de origen y la página de destino
+        sessionStorage.setItem("origen", "index.html");
+        sessionStorage.setItem("destino", "datos.html");
+        // Guardamos localmente el mensaje que vamos a mostrar por encima del formulario
+        sessionStorage.setItem("msgLogin", "Para acceder a la sección de datos, debe iniciar sesión");
+        // Redirigimos a login.html (página de inicio de sesión)
+        window.location.href = "pages/login.html";
+    }
+});
+
+/* Definimos un evento que se dispara cada vez que se presiona el enlace para
+iniciar sesión */
+let linkLogin = document.getElementById("login-link");
+linkLogin.addEventListener("click", function(){
+    // Definimos la página de origen y la página de destino
+    sessionStorage.setItem("origen", "index.html");
+    sessionStorage.setItem("destino", "index.html");
+})
+
+if(sessionStorage.getItem("sesionIniciada") == "true"){
+    // Eliminamos el enlace para iniciar sesión y lo reemplazamos por un ícono junto al nombre de usuario
+    document.getElementById("login-link").remove();
+    let usrIcon = document.createElement("i");
+    usrIcon.className = "fa-solid fa-user";
+    usrIcon.style.marginLeft = "32px";
+    usrIcon.href = "#";
+    document.getElementById("login-item").appendChild(usrIcon);
+}
