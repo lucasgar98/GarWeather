63ab9bf619ec308b38cc1d0c00dc7289682209c4
lucasgar98 <lucas_garbarino@hotmail.com>
2025-12-11 22:23:57 -0300

    Se agregó una función para buscar el usuario ingreado en el almacenamiento local

--- js/login.js
+++ js/login.js
@@ -1,8 +1,4 @@
 //---------- VALIDACIÓN DEL FORMULARIO DE INICIO DE SESIÓN -----------
-// Definimos el usuario y contraseña válidos de forma "hardcodeada"
-const usrVal = "lucasgar98";
-const passVal = "REINAquerida2025";
-
 // Función que muestra un mensaje de error encima del formulario
 function mostrarError(message){
     // Accedemos al contenedor que se encuentra por encima del formulario
@@ -20,6 +16,62 @@ function ocultarError() {
     //console.log("Usuario y contraseña válidos");
     // Modificamos la propiedad display para ocultar el mensaje de error
     errorElement.style.display = "none";
+}
+
+/* Función que busca el nombre de usuario en el almacenamiento local (localStorage) */
+function buscarUsuario(nombre, contraseña){
+    let usuarioEncontrado;
+    // Buscamos el nombre de usuario y la contraseña en el localStorage
+    const usuarios = JSON.parse(localStorage.getItem("cuenta-usuario"));
+
+    if(usuarios != null){
+        if(usuarios.length == undefined){
+            if(usuarios.usuario == nombre){
+                if(usuarios.contraseña == contraseña){
+                    // Usuario y contraseña válidos
+                    usuarioEncontrado = true;
+                }
+                else {
+                    // Usuario válido, contraseña inválida
+                    mostrarError("La contraseña es incorrecta");
+                    return false;
+                }
+            }
+            else {
+                // El usuario no existe
+                mostrarError("El usuario no existe");
+                return false;
+            }
+        }
+        else if(usuarios.length > 1) {
+            for(let i=0; i<usuarios.length; i++){
+                if(usuarios[i].usuario == nombre){
+                    if(usuarios[i].contraseña == contraseña){
+                        // Usuario y contraseña válidos
+                        usuarioEncontrado = true;
+                    }
+                    else {
+                        // Usuario válido, contraseña inválida
+                        mostrarError("La contraseña es incorrecta");
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    else {
+        usuarioEncontrado = false;
+    }
+
+    if(usuarioEncontrado == true){
+        ocultarError();
+        return true;
+    }
+    else {
+        mostrarError("El usuario no existe");
+        return false;
+    }
+
 }
 
 // Función para validar los datos ingresados por el usuario al iniciar sesión
@@ -46,53 +98,52 @@ function validarLogin() {
         mostrarError("La contraseña no puede estar vacía");
         esValido = false;
     }
-    else if(usrName != usrVal || usrPass != passVal){
-        // Usuario o contraseña inválidos
-        mostrarError("Credenciales inválidas");
-        esValido = false;
-    }
-    else if(usrName == usrVal && usrPass == passVal){
-        // Usuario y contraseña válidos
-        ocultarError();
-        esValido = true;
-    }
-
+    esValido = buscarUsuario(usrName, usrPass);
     return esValido;
 }
 
-// Mostramos el mensaje por encima del formulario de inicio de sesión 
-const msgLogin = sessionStorage.getItem("msgLogin");
-if(msgLogin) {
-    let txtLogin = document.getElementById("txt-login");
-    txtLogin.innerHTML = `<h2>${msgLogin}</h2> `;
-    // Borramos el mensaje para que no reaparezca
-    sessionStorage.removeItem("msgLogin");
-}
-
-/* Definimos un evento que se dispara cada vez que se presiona el botón acceder.
-Cuando se dispara este evento, se realiza la validación del formulario */
-let btnAcceder = document.getElementById("but-login");
-btnAcceder.addEventListener("click", function(event) {
-    event.preventDefault(); // Evitamos que el documento HTML maneje el evento por defecto
-    //console.log("Botón Acceder presionado");
-    if(validarLogin()){
-        // Obtenemos la página de destino
-        //const origen = sessionStorage.getItem("origen");
-        const destino = sessionStorage.getItem("destino");
-        // Definimos la página que se abrirá al presionar el botón acceder, según la página de destino
-        if(destino == "datos.html"){
-            window.location.href = "../pages/datos.html";
-        }
-        else if(destino == "index.html"){
-            window.location.href = "../index.html";
-        }
-        else if(destino == "contacto.html"){
-            window.location.href = "../pages/contacto.html";
-        }
-        else if(destino == "nosotros.html"){
-            window.location.href = "../pages/nosotros.html";
-        }
-        
-        sessionStorage.setItem("sesionIniciada", "true");
+// Mostramos el mensaje por encima del formulario de inicio de sesión
+if(document.URL.includes("login.html")){
+    const msgLogin = sessionStorage.getItem("msgLogin");
+    if(msgLogin) {
+        let txtLogin = document.getElementById("txt-login");
+        txtLogin.innerHTML = `<h2>${msgLogin}</h2> `;
+        // Borramos el mensaje para que no reaparezca
+        sessionStorage.removeItem("msgLogin");
     }
-});
+
+    /* Definimos un evento que se dispara cada vez que se presiona el botón acceder.
+    Cuando se dispara este evento, se realiza la validación del formulario */
+    let btnAcceder = document.getElementById("but-login");
+    btnAcceder.addEventListener("click", function(event) {
+        event.preventDefault(); // Evitamos que el documento HTML maneje el evento por defecto
+        //console.log("Botón Acceder presionado");
+        if(validarLogin()){
+            // Obtenemos la página de destino
+            //const origen = sessionStorage.getItem("origen");
+            const destino = sessionStorage.getItem("destino");
+            // Definimos la página que se abrirá al presionar el botón acceder, según la página de destino
+            if(destino == "datos.html"){
+                window.location.href = "../pages/datos.html";
+            }
+            else if(destino == "index.html"){
+                window.location.href = "../index.html";
+            }
+            else if(destino == "contacto.html"){
+                window.location.href = "../pages/contacto.html";
+            }
+            else if(destino == "nosotros.html"){
+                window.location.href = "../pages/nosotros.html";
+            }
+            
+            sessionStorage.setItem("sesionIniciada", "true");
+        }
+    });
+
+    /* Evento que se dispara al presionar el botón de crear cuenta */
+    const btnSignUp = document.getElementById("but-signup");
+    btnSignUp.addEventListener("click", function(event) {
+        event.preventDefault(); // Evitamos que el documento HTML maneje el evento por defecto
+        window.location.href = "signup.html";
+    });
+}
